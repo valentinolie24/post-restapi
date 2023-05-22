f2ce1f6d35a4c100aa11615e7eeeb34bb4339259
valentinolie24 <72203537+valentinolie24@users.noreply.github.com>
2023-05-22 11:30:37 +0700

    Konfigurasi

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('PILAT KAU!')
+  res.send('KOCAK')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
